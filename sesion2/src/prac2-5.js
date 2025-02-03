133d5bdc34b8de5fb7fffc52e4956109de308b51
UO237050 <UO237050@uniovi.es>
2025-02-03 15:46:01 +0100

    Practica 2.5 sin acabar

---
+++ sesion2/src/prac2-5.js
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+camera.position.set( 0, 0, 500 );
+
+const RTierra= 50;
+const tierraGeometry= new THREE.SphereGeometry(RTierra,32,32);
+const mapUrl = "../textures/earth.jpg";   // The file used as texture
+const textureLoader = new THREE.TextureLoader( );  // The object used to load textures
+const map = textureLoader.load( mapUrl, ( loaded ) => { renderer.render( scene, camera ); } );
+const material = new THREE.MeshPhongMaterial( { map: map } );
+const tierra = new THREE.Mesh(tierraGeometry,material);
+
+const RAtmos=RTierra+1;
+const atmosGeometry= new THREE.SphereGeometry(RAtmos,32,32);
+const mapUrlatmos = "../textures/atmos.png";   // The file used as texture
+const textureLoaderatmos = new THREE.TextureLoader( );  // The object used to load textures
+const mapAtmos = textureLoaderatmos.load( mapUrlatmos, ( loaded ) => { renderer.render( scene, camera ); } );
+var atmosphereMaterial = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, map: mapAtmos, transparent: true } );
+
+const atmos = new THREE.Mesh(atmosGeometry,atmosphereMaterial);
+scene.add(tierra);
+scene.add(atmos);
+
+const grupoTierra= new THREE.Object3D();
+grupoTierra.add(tierra);
+grupoTierra.add(atmos);
+grupoTierra.rotateZ(0.36);
+scene.add(grupoTierra);
+
+const RMoon=0.27*RTierra;
+const moonGeometry= new THREE.SphereGeometry(RMoon,32,32);
+const distance= RTierra*60.3;
+const moonMapUrl = 'moon.jpg';
+const moonMap = textureLoader.load( moonMapUrl, ( loaded ) => { renderer.render( scene, camera ); } );
+const moonMaterial = new THREE.MeshLambertMaterial( { map: moonMap, color: 0x888888 } );
+const moon= new THREE.Mesh(moonGeometry,moonMaterial);
+moon.position.set( Math.sqrt( distance / 2 ) * RTierra, 0, -Math.sqrt( distance / 2 ) * RTierra );
+// Rotate the Moon to face visible side to the Earth (tidal locking)
+moon.rotation.y = Math.PI;
+
+scene.add(moon);
+// Moon should rotate around the Earth: an Object3D is needed
+const moonGroup = new THREE.Object3D( );
+moonGroup.add( moon );
+
+// The Moon orbit is a bit tilted
+moonGroup.rotation.x = 0.089;
+scene.add(moonGroup);
+
+const ambientLight = new THREE.AmbientLight(0x404040); 
+scene.add(ambientLight);
+
+const pointLight= new THREE.PointLight(0xFFFFFF, 1,5);
+pointLight.position.set(100, 50, 0);
+scene.add(pointLight);
+
+
+
+renderer.render( scene, camera );
+
+window.addEventListener( 'resize', ( ) => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+}, false );
