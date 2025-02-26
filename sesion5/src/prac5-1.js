c66537b428063dd8b09e83dcba1077874815208f
UO237050 <UO237050@uniovi.es>
2025-02-26 23:10:22 +0100

    Prac5-1 hecha

--- sesion5/src/prac5-1.js
+++ sesion5/src/prac5-1.js
@@ -3,3 +3,6 @@ import * as dashjs from '../dash.all.min.js';
 const url = "../videos/trailer_extraido.mpd";
 const player = dashjs.MediaPlayer().create();
 player.initialize(document.querySelector("#player"), url, true);
+
+document.querySelector("#player").style.display="block";
+
