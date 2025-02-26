51760f30de8ceec188f82be6b3735b244573bb41
UO237050 <UO237050@uniovi.es>
2025-02-26 15:23:35 +0100

    cambios hechos, pero sin acabar

--- sesion5/src/prac5-1.js
+++ sesion5/src/prac5-1.js
@@ -1,1 +1,5 @@
-import dash.all.min.js;
+import * as dashjs from '../dash.all.min.js';
+
+const url = "../videos/trailer_extraido.mpd";
+const player = dashjs.MediaPlayer().create();
+player.initialize(document.querySelector("#player"), url, true);
