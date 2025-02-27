b0bde104dee79f4f7b963b8e1752d0e57e355243
UO237050 <UO237050@uniovi.es>
2025-02-27 19:57:23 +0100

    Practica 6 acabada

---
+++ sesion6/src/prac6-4.js
@@ -0,0 +1,110 @@
+import * as THREE from 'three';
+import adapter from 'webrtc-adapter';
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+camera.position.set( 0, 0, 300 );
+
+const video = document.getElementById( 'video' );
+const constraints = {
+    audio: false,
+    video: { width: { exact: 640 }, height: { exact: 480 } }
+};
+
+navigator.mediaDevices.getUserMedia( constraints )
+    // Called when we get the requested streams
+    .then( ( stream ) => {
+
+        // Video tracks (usually only one)
+        const videoTracks = stream.getVideoTracks( );
+        console.log( 'Stream characteristics: ', constraints );
+        console.log( 'Using device: ' + videoTracks[0].label );
+
+        // End of stream handler
+        stream.onended = () => {
+
+            console.log( 'End of stream' );
+        };
+
+        // Bind the stream to the html video element
+        video.srcObject = stream;
+})
+    // Called in case of error
+    .catch( ( error ) => {
+
+        if ( error.name === 'ConstraintNotSatisfiedError' ) {
+
+            console.error( 'The resolution ' + constraints.video.width.exact + 'x' +
+                          constraints.video.width.exact + ' px is not supported by the camera.' );
+        } else if ( error.name === 'PermissionDeniedError' ) {
+
+            console.error( 'The user has not allowed the access to the camera and the microphone.' );
+        }
+        console.error( ' Error in getUserMedia: ' + error.name, error );
+});
+
+const image = document.createElement( 'canvas' );
+image.width = 480;  // Video width
+image.height = 204; // Video height
+const imageContext = image.getContext( '2d' );
+imageContext.fillStyle = '#000000';
+imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
+const texture = new THREE.Texture( image );
+
+const material = new THREE.MeshBasicMaterial( { map: texture } );
+const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
+
+scene.add(wall);
+
+
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', () => init(), false);
+
+function init() {
+    var overlay = document.getElementById('overlay');
+    overlay.remove();
+
+    // Do stuff
+    
+    video.play();
+}
+
+const clock = new THREE.Clock( );
+
+function animate( ) {
+
+    const delta = clock.getDelta( ); // Elapsed time in seconds
+
+    // UPDATE THE SCENE ACCORDING TO THE ELAPSED TIME
+    const rotation = ( delta * Math.PI * 2 ) / 24;
+    wall.rotation.y += rotation;
+    if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+
+        imageContext.drawImage( video, 0, 0 );
+        if ( texture ) texture.needsUpdate = true;
+    }
+    // Render the scene
+    renderer.render( scene, camera );
+
+    // Request the browser to execute the animation-rendering loop
+    requestAnimationFrame( animate );
+};
+animate();
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+directionalLight.position.set(100, 0, 200);
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0x404040, 0.5); 
+scene.add(ambientLight);
+
+window.addEventListener( 'resize', ( ) => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( scene, camera );
+}, false );
